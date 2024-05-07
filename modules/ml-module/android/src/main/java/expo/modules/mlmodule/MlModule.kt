fdc5be19c777b40f1ecddd34832a3b0099399bce
Soumith Reddy <soumithreddy51155@gmail.com>
2024-05-07 08:18:54 +0530

    initial setup

--- modules/ml-module/android/src/main/java/expo/modules/mlmodule/MlModule.kt
+++ modules/ml-module/android/src/main/java/expo/modules/mlmodule/MlModule.kt
@@ -1,39 +1,40 @@
 package expo.modules.mlmodule
 
+
+import android.util.Log
+import expo.modules.kotlin.Promise
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import expo.modules.kotlin.records.Field
+import expo.modules.kotlin.records.Record
+import org.opencv.android.OpenCVLoader
 
 class MlModule : Module() {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
   override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('MlModule')` in JavaScript.
+
     Name("MlModule")
 
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello world! 👋"
+    AsyncFunction("predict") { image: String, options: Options , promise: Promise ->
+      val result = DetectionResult()
+
+
+      promise.resolve(result)
     }
 
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
-    }
 
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
-    View(MlModuleView::class) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { view: MlModuleView, prop: String ->
-        println(prop)
-      }
-    }
+
   }
+}
+
+class Options : Record {
+  @Field
+  val verbose: Boolean = false
+}
+
+class DetectionResult:Record{
+  @Field
+  var positions = Array(8) { Array<String?>(8) { null } }
+
+  @Field
+  var boardResult:String? = null
 }
